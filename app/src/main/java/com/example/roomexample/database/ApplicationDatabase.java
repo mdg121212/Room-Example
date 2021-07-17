7823532d50a2985c955536e9c49fbd183c9ab986
Matt G <63030131+mdg121212@users.noreply.github.com>
2021-07-17 07:13:24 -0700

    more stuff

---
+++ app/src/main/java/com/example/roomexample/database/ApplicationDatabase.java
@@ -0,0 +1,5 @@
+package com.example.roomexample.database;
+
+public class ApplicationDatabase {
+
+}
--- app/src/main/java/com/example/roomexample/ui/MainActivity.java
+++ app/src/main/java/com/example/roomexample/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.roomexample;
+package com.example.roomexample.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.roomexample.R;
 
 public class MainActivity extends AppCompatActivity {
 
